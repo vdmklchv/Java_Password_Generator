0694a86814c7463653c6a9202b119d85704adb59
Vadim Colcev <vadimkolchev@gmail.com>
2022-05-06 23:11:46 +0300

    Initial release

---
+++ Main.java
@@ -0,0 +1,114 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int uppercase = sc.nextInt();
+        int lowercase = sc.nextInt();
+        int digits = sc.nextInt();
+        int length = sc.nextInt();
+
+
+        StringBuilder password = new StringBuilder();
+
+
+        CharType currentCharType = null;
+        String currentElement = null;
+
+
+        for (int i = 0; i < length; i++) {
+            while (uppercase > 0) {
+                String newElement = getRandomElement(CharType.UPPER);
+                while (Objects.equals(newElement, currentElement)) {
+                    newElement = getRandomElement(CharType.UPPER);
+                }
+                currentElement = newElement;
+                password.append(currentElement);
+                uppercase--;
+                currentCharType = CharType.UPPER;
+            }
+            while (lowercase > 0) {
+                String newElement = getRandomElement(CharType.LOWER);
+                while (Objects.equals(newElement, currentElement)) {
+                    newElement = getRandomElement(CharType.LOWER);
+                }
+                currentElement = newElement;
+                password.append(currentElement);
+                lowercase--;
+                currentCharType = CharType.LOWER;
+            }
+            while (digits > 0) {
+                String newElement = getRandomElement(CharType.DIGIT);
+                while (Objects.equals(newElement, currentElement)) {
+                    newElement = getRandomElement(CharType.DIGIT);
+                }
+                currentElement = newElement;
+                password.append(currentElement);
+                digits--;
+                currentCharType = CharType.DIGIT;
+            }
+
+            int remaining = length - password.length();
+
+            for (int j = 0; j < remaining; j++) {
+                CharType newType = getRandomType();
+                while (Objects.equals(newType, currentCharType)) {
+                    newType = getRandomType();
+                }
+                currentCharType = newType;
+                String newElement = getRandomElement(currentCharType);
+                while (Objects.equals(newElement, currentElement)) {
+                    newElement = getRandomElement(currentCharType);
+                }
+                currentElement = newElement;
+                password.append(currentElement);
+            }
+
+
+        }
+
+        System.out.println(password);
+    }
+
+    private static String getRandomElement(CharType type) {
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+        String nums = "1234567890";
+
+        int index;
+        switch (type) {
+            case DIGIT:
+                index = (int) (Math.random() * nums.length());
+                return String.valueOf(nums.charAt(index));
+            case UPPER:
+                index = (int) (Math.random() * chars.length());
+                return String.valueOf(chars.charAt(index)).toUpperCase();
+            case LOWER:
+                index = (int) (Math.random() * chars.length());
+                return String.valueOf(chars.charAt(index));
+            default:
+                System.out.println("Something wrong with characters");
+                return null;
+        }
+    }
+
+    private static CharType getRandomType() {
+        int randIndex = (int) (Math.random() * 3);
+        switch (randIndex) {
+            case 0:
+                return CharType.UPPER;
+            case 1:
+                return CharType.LOWER;
+            case 2:
+                return CharType.DIGIT;
+            default:
+                System.out.println("Illegal type");
+                return null;
+        }
+    }
+
+    enum CharType {
+        UPPER, LOWER, DIGIT
+    }
+}
